9360b684f07ef76171a9dd3a5b5e120f66ab9fb8
Salavat Yalalov <s.yalalov@icloud.com>
2021-01-06 20:10:56 +0500

    events

--- app.js
+++ app.js
@@ -11,6 +11,32 @@ const template = (
         <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
     </div>
 );
+
+let count = 0
+const addOne = () => {
+    count++;
+    console.log(count);
+}
+
+const minusOne = () => {
+    count--;
+    console.log(count);
+}
+
+const resetToZero = () => {
+    count = 0;
+    console.log(count);
+}
+
+const templateTwo = (
+    <div>
+        <h1>Count: {count}</h1>
+        <button onClick={addOne}>+1</button>
+        <button onClick={minusOne}>-1</button>
+        <button onClick={resetToZero}>Reset</button>
+    </div>
+);
+
 const appRoot = document.getElementById('app')
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -23,6 +23,49 @@ const template = React.createElement(
         app.options.length > 0 ? 'Here are your options' : 'No options'
     )
 );
+
+let count = 0;
+const addOne = () => {
+    count++;
+    console.log(count);
+};
+
+const minusOne = () => {
+    count--;
+    console.log(count);
+};
+
+const resetToZero = () => {
+    count = 0;
+    console.log(count);
+};
+
+const templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Count: ',
+        count
+    ),
+    React.createElement(
+        'button',
+        { onClick: addOne },
+        '+1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: minusOne },
+        '-1'
+    ),
+    React.createElement(
+        'button',
+        { onClick: resetToZero },
+        'Reset'
+    )
+);
+
 const appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
